ae28833378ef6f50481e5d47b4a9371773eb55a3
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-22 14:47:09 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,13 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  if passengers['suite_a'][0] == "A"
+    return passengers['suite_a']
+  end
 end
